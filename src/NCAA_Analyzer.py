8dd9c486fd593866fe2270bd833e664e66385764
Haoxiang Yang <haoxiangyang89@gmail.com>
2018-08-05 21:58:01 -0500

    Updated the Bayesian learning process

--- src/NCAA_Analyzer.py
+++ src/NCAA_Analyzer.py
@@ -16,7 +16,7 @@ import sklearn
 
 class dataEntry:
     def __init__(self,gdate,hTeam,aTeam,neutral,specTour,confTour,gameStat):
-        self.gdate = gdate
+        self.gDate = gdate
         self.hTeam = hTeam
         self.aTeam = aTeam
         self.neutral = neutral
@@ -61,7 +61,6 @@ def parseRaw(title,gameList):
     
     gameID = 0
     dataList = []
-    totalData = np.array([])
     for item in gameList:
         dataItem = []
         
@@ -96,13 +95,13 @@ def parseRaw(title,gameList):
         h3ptmade = int(item[title.index("Home 3PT Made")])
         a3ptmade = int(item[title.index("Away 3PT Made")])
         
-        h3ptPerc = float(item[title.index("Home 3PT Percentage")])
-        a3ptPerc = float(item[title.index("Away 3PT Percentage")])
+        h3ptPerc = float(item[title.index("Home 3PT Percentage")])/100
+        a3ptPerc = float(item[title.index("Away 3PT Percentage")])/100
         h2ptPerc = (hFGmade - h3ptmade)/(hFGattempt - h3ptattempt)
         a2ptPerc = (aFGmade - a3ptmade)/(aFGattempt - a3ptattempt)
         
-        hFTPerc = float(item[title.index("Home FT Percentage")])
-        aFTPerc = float(item[title.index("Away FT Percentage")])
+        hFTPerc = float(item[title.index("Home FT Percentage")])/100
+        aFTPerc = float(item[title.index("Away FT Percentage")])/100
         
         hFoul = int(item[title.index("Home PF")])
         aFoul = int(item[title.index("Away PF")])
@@ -125,20 +124,75 @@ def parseRaw(title,gameList):
         
         dataItem = dataEntry(gameDate,homeID,awayID,neutral,specTour,confTour,gamestat)
         dataList.append(dataItem)
+      
+    dataList = sorted(dataList,key = lambda k:k.gDate)
+    totalData = np.array([])
+    for item in dataList:
         if totalData.size == 0:
-            totalData = np.array([gamestat])
+            totalData = np.array([item.gameStat])
         else:
-            totalData = np.append(totalData,np.array([gamestat]),axis = 0)
+            totalData = np.append(totalData,np.array([item.gameStat]),axis = 0)
         
     return teamID,dataList,totalData
 
-def bayesLearn(teamID,dataList):
+def bayesLearn(teamID,dataList,totalData):
     # artificial variance
     avgStat = np.mean(totalData,axis = 0)
     varStat = avgStat*0.05
     varMat = np.diag(varStat**2)
     
     # initialization with every team's theta prior
-    thetaDict = {}
+    thetamDict = {}
+    thetasDict = {}
+    OCA = (avgStat[0] + avgStat[1])/2
+    OSA = 0
+    twoAccu = (avgStat[2] + avgStat[3])/2
+    twoD = 0
+    threeAccu = (avgStat[4] + avgStat[5])/2
+    threeD = 0
+    ftAccu = (avgStat[6] + avgStat[7])/2
+    FCA = (avgStat[8] + avgStat[9])/2
+    DC = 0
+    TOCA = (avgStat[10] + avgStat[11])/2
+    PTO = (avgStat[10] + avgStat[11])/2
+    BLKA = (avgStat[12] + avgStat[13])/2
+    PBLK = (avgStat[12] + avgStat[13])/2
+    ORA = (avgStat[14] + avgStat[15])/2
+    DRA = 0
+    
     for tID in teamID.keys():
-        thetaDict[tID] = np.array([])
+        thetamDict[tID] = np.array([OCA,OSA,twoAccu,twoD,threeAccu,threeD,ftAccu,FCA,DC,\
+                 TOCA,PTO,BLKA,PBLK,ORA,DRA])
+        thetasDict[tID] = np.array([OCA*0.05,OCA*0.05,twoAccu*0.05,twoAccu*0.05,threeAccu*0.05,threeAccu*0.05,\
+                  ftAccu*0.05,FCA*0.05,FCA*0.05,TOCA*0.05,PTO*0.05,BLKA*0.05,PBLK*0.05,ORA*0.05,ORA*0.05])**2
+    
+    A = np.matrix([[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+                  [0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+                  [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0],\
+                  [0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],\
+                  [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0],\
+                  [0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],\
+                  [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],\
+                  [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0],\
+                  [0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],\
+                  [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],\
+                  [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],\
+                  [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],\
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],\
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]])
+    
+    # Bayesian learning process
+    for item in dataList:
+        # update the variance for the theta
+        joints = np.append(thetasDict[item.hTeam],thetasDict[item.aTeam])
+        sigma = np.linalg.inv(np.linalg.inv(np.diag(joints))+np.transpose(A)*np.linalg.inv(varMat)*A)
+        thetasDict[item.hTeam] = sigma[:14,:14]
+        thetasDict[item.aTeam] = sigma[15:,15:]
+        
+        # update the mean for the theta
+        jointm = np.append(thetamDict[item.hTeam],thetamDict[item.aTeam])
+        mu = sigma*(jointm*np.linalg.inv(np.diag(joints))+np.transpose(A)*np.linalg.inv(varMat)*item.gameStat)
+        thetamDict[item.hTeam] = mu[:14]
+        thetamDict[item.aTeam] = mu[15:]
